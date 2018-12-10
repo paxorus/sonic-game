8ede45f9c7354f3cd41985f93397f559807379a2
Prakhar Sahay <prakhar.studios@gmail.com>
2018-12-09 19:33:03 -0500

    Allow Left-Right Switching and Prevent Re-Running

--- main.js
+++ main.js
@@ -28,24 +28,23 @@ const INITIAL_LOCUS = [16, 41, 0];
 const JUMPING_LOCUS = [12, 550, 0];
 const FALLING_LOCUS = [485, 480, 0];
 const STEP_SIZE = 126;
-const JUMP_HEIGHT = 25;
+const JUMP_HEIGHT = 200;
 const JUMP_SPEED = 8;
 
 const RUNNING_LOCI = [
 	[429, 104, 0],// right heel up
 	[472, 104, 10],// right foot up
-	[516, 104, 11],// right foot curve
-	[561, 104, 12],// right toe up
-	[612, 104, 18],// right toe way up
-	[665, 104, 21],// right heel down
-	[16, 168, 41],// right foot down
-	[64, 168, 62],// left foot down
-	[106, 168, 65],// left foot up
-	[148, 168, 65],// left foot curve
-	[190, 168, 84],// left toe semi-up
-	[245, 168, 88],// left heel down
-	[308, 168, 128],// left foot down
-	[16, 41, 126]// at rest
+	[516, 104, 1],// right foot curve
+	[561, 104, 1],// right toe up
+	[612, 104, 6],// right toe way up
+	[665, 104, 3],// right heel down
+	[16, 168, 20],// right foot down
+	[64, 168, 21],// left foot down
+	[106, 168, 3],// left foot up
+	[148, 168, 0],// left foot curve
+	[190, 168, 19],// left toe semi-up
+	[245, 168, 4],// left heel down
+	[308, 168, 30]// left foot down
 ];
 
 class Sonic {
@@ -53,10 +52,13 @@ class Sonic {
 		this.locus = INITIAL_LOCUS;
 		this.x = 0;
 		this.y = 0;
+		this.vx = 0;
+		this.vy = 0;
 		this.width = 40;
 		this.height = 50;
 		this.scale = 2;
 		this.isFacingRight = true;
+		this.walkFrame = 0;
 
 		this.sprite = this.getSpriteSheet('sonic_3_custom_sprites_by_facundogomez-dawphra.png');
 		this.sprite.onload = () => {
@@ -81,28 +83,26 @@ class Sonic {
 				- locus[2] - 20 // flipping adjustment
 			];
 		}
-		const pixelX = locus[2] + this.x * STEP_SIZE;
-		const pixelY = this.y * JUMP_SPEED;
 
 		const sprite = this.isFacingRight ? this.sprite : this.reverseSprite;
-		canvas.drawImage(sprite, locus, [pixelX, pixelY], this.scale);
+		canvas.drawImage(sprite, locus, [this.x, this.y], this.scale);
 	}
 
 	moveRight() {
 		let frame = 1;
 		this.isFacingRight = true;
+		this.vx = 1;
 
 		const _moveRight = () => {
+			console.log('go right!');
 			this.locus = RUNNING_LOCI[frame];
+			this.x += this.locus[2];
+
 			canvas.clear();
 			this.draw();
-			if (frame + 1 < RUNNING_LOCI.length) {
-				frame ++;
-				requestAnimationFrame(_moveRight);
-			} else {
-				this.locus = INITIAL_LOCUS;
-				this.x ++;
-			}
+
+			frame = (frame + 1) % RUNNING_LOCI.length;
+			this.walkFrame = requestAnimationFrame(_moveRight);
 		};
 
 		_moveRight();
@@ -111,18 +111,17 @@ class Sonic {
 	moveLeft() {
 		let frame = 1;
 		this.isFacingRight = false;
+		this.vx = -1;
 
 		const _moveLeft = () => {
 			this.locus = RUNNING_LOCI[frame];
+			this.x -= this.locus[2];
+
 			canvas.clear();
 			this.draw();
-			if (frame + 1 < RUNNING_LOCI.length) {
-				frame ++;
-				requestAnimationFrame(_moveLeft);
-			} else {
-				this.locus = INITIAL_LOCUS;
-				this.x --;
-			}
+
+			frame = (frame + 1) % RUNNING_LOCI.length;
+			this.walkFrame = requestAnimationFrame(_moveLeft);
 		};
 
 		_moveLeft();
@@ -130,7 +129,7 @@ class Sonic {
 
 	jump() {
 		const _moveUp = () => {
-			this.y ++;
+			this.y += JUMP_SPEED;
 			canvas.clear();
 			this.draw();
 
@@ -145,7 +144,7 @@ class Sonic {
 		};
 
 		const _moveDown = () => {
-			this.y --;
+			this.y -= JUMP_SPEED;
 			canvas.clear();
 			this.draw();
 			if (this.y > 0) {
@@ -161,6 +160,16 @@ class Sonic {
 		_moveUp();
 	}
 
+	pause() {
+		cancelAnimationFrame(this.walkFrame);
+		this.walkFrame = null;
+		this.vx = 0;
+		this.locus = INITIAL_LOCUS;
+
+		canvas.clear();
+		this.draw();
+	}
+
 }
 
 const canvas = new Canvas('canvas');
@@ -172,11 +181,36 @@ document.addEventListener('keydown', (ev) => {
 		case 32:// Space
 			sonic.jump();
 			break;
-		case 37:// Reverse images for moving left.
-			sonic.moveLeft();
+		case 37:// Left
+			if (sonic.vx === 0) {
+				sonic.moveLeft();
+			} else if (sonic.vx === 1) {
+				// Interrupt right.
+				sonic.pause();
+				sonic.moveLeft();
+			}
 			break;
 		case 39:// Right
-			sonic.moveRight();// Needs to be uninterruptible.
-			// Should not come to rest if key still down.
+			if (sonic.vx === 0) {
+				sonic.moveRight();
+			} else if (sonic.vx === -1) {
+				sonic.pause();
+				sonic.moveRight();
+			}
+	}
+});
+
+document.addEventListener('keyup', (ev) => {
+	switch (ev.keyCode) {
+		case 37:
+			if (sonic.vx === -1) {
+				sonic.pause();
+			}
+			break;
+		case 39:
+			if (sonic.vx === 1) {
+				sonic.pause();
+			}
+			break;
 	}
 });
